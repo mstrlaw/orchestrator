7c752e42472a301d64bd8ea226a49a98d162f91e
Lawrence Almeida <lawrence.almeida@unbabel.com>
2020-05-17 16:35:11 +0100

    Register apps

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 import {
   start,
+  registerApplication
 } from 'single-spa';
+
+const apps = [
+  {
+    name: 'app-one',
+    app: () => System.import('app-one'),
+    activeWhen: location => location.pathname.startsWith('/')
+  },
+  {
+    name: 'app-two',
+    app: () => System.import('app-two'),
+    activeWhen: location => location.pathname.startsWith('/')
+  }
+]
 
 Promise.all([
   System.import('pubsub-js'),
   System.import('snackbar')
 ]).then(() => {
-  
+  apps.forEach(app =>  registerApplication(app) );
   start();
-
-  Snackbar.show({
-    text: 'Single SPA loaded!'
-  });
 });
